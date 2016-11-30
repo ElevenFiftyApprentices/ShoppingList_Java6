1aaf8c1b2852adceb4bdca9356c2fcd23c34c30e
Anthony Holt <Anthony Holt>
2016-11-30 10:20:37 -0500

    Added in a basic controller and repository, as well as an index html. now need to work with Sydney to ensure SQL is correctly working.

--- src/main/java/org/elevenfifty/shopping/repository/ShoppingListRepository.java
+++ src/main/java/org/elevenfifty/shopping/repository/ShoppingListRepository.java
@@ -1,5 +1,11 @@
 package org.elevenfifty.shopping.repository;
 
-public class ShoppingListRepository {
+
+import org.elevenfifty.shopping.beans.ShoppingList;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ShoppingListRepository extends CrudRepository<ShoppingList, Integer>{
 
 }
