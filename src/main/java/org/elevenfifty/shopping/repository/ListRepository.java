5d2c090b60acf8ed852d9862335051a5f303a665
Anthony Holt <Anthony Holt>
2016-12-01 10:31:24 -0500

    finally able to get the list of lists to display, and refactored the project's naming conventions to be easier to read and type

--- src/main/java/org/elevenfifty/shopping/repository/ListRepository.java
+++ src/main/java/org/elevenfifty/shopping/repository/ListRepository.java
@@ -1,11 +1,11 @@
 package org.elevenfifty.shopping.repository;
 
 
-import org.elevenfifty.shopping.beans.ShoppingList;
+import org.elevenfifty.shopping.beans.List;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShoppingListRepository extends CrudRepository<ShoppingList, Integer>{
+public interface ListRepository extends CrudRepository<List, Integer>{
 
 }
