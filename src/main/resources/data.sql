de6a574f362c173743ad7a9ad95ac81d618b7ea4
Sydney Haggard <sydneyellen1996@gmail.com>
2016-12-05 16:09:09 -0500

    fixed sql errors

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,3 @@
-insert into heroku_954c820b76f9b7e.lists (user_id, name, color, created_utc, modified_utc) values (1, 'Test List', '#000000', '2016-11-30 03:42:56', '2016-11-30 03:42:56');
+insert into heroku_954c820b76f9b7e.lists (user_id, name, color, is_checked, created_utc, modified_utc) values (1, 'Test List', '#000000', false ,'2016-11-30 03:42:56', '2016-11-30 03:42:56');
 
 insert into heroku_954c820b76f9b7e.list_items (list_id, contents, priority, is_checked, created_utc, modified_utc) values (1, 'Hello there', 1, false, '2016-11-30 03:42:56', '2016-11-30 03:42:56');
