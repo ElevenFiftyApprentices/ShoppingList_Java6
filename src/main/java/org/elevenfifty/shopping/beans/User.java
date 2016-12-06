2d17ace788870e0d3533f504505dd5b682f4f84b
Anthony Holt <biggestantman@gmail.com>
2016-12-06 12:28:15 -0500

    almost forgot @entity on the user bean

--- src/main/java/org/elevenfifty/shopping/beans/User.java
+++ src/main/java/org/elevenfifty/shopping/beans/User.java
@@ -1,11 +1,13 @@
 package org.elevenfifty.shopping.beans;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.Size;
 
+@Entity
 public class User {
 	// adding in users for possible gold challenge requirements.
 	@Id
