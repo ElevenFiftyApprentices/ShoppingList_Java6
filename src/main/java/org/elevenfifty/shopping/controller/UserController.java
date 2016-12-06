29f29bfa3d3ea8ceac1bfc008c355790fa93c20a
Anthony Holt <biggestantman@gmail.com>
2016-12-06 08:40:53 -0500

    added in a login html and method to control. Will need to discuss with Sydney about adding Users to the Database.

--- src/main/java/org/elevenfifty/shopping/controller/UserController.java
+++ src/main/java/org/elevenfifty/shopping/controller/UserController.java
@@ -4,11 +4,17 @@ package org.elevenfifty.shopping.controller;
 import org.elevenfifty.shopping.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
 	@Autowired
 	private UserRepository userRepo;
-	
+
+	@GetMapping("/login")
+	public String login(Model model) {
+		return "login";
+	}
 }
